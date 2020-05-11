32bcad9c4348e9c07c3dbe6f99419b0516788f6e
Ilya Kolebayev <kolebayev@gmail.com>
2020-05-11 23:00:41 +0300

    Add login on server side

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
-console.clear()
+console.clear();
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
 const port = process.env.PORT || 5000;
 
-const parseAppleLink = require("./routes/parseApplePlaylistLink");
+const parseAppleLink = require('./routes/parseApplePlaylistLink');
+const login = require('./routes/login');
 
 app.listen(port);
+
 console.log(`listening on ${port} \n`);
 
-app.use("/", parseAppleLink);
+app.use('/', parseAppleLink);
+app.use('/', cors(), login);
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const spotify = require('../spotify/creds.js');
+const querystring = require('querystring');
 const router = express.Router();
+const cors = require('cors');
+// const corsOptions = {
+//   origin: 'http://localhost:3000/',
+// };
 
-router.get('/login', (req, res) => {
-  var scopes = 'user-read-private user-read-email';
-  res.redirect(
-    'https://accounts.spotify.com/authorize' +
-      '?response_type=code' +
-      '&client_id=' +
-      my_client_id +
-      (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-      '&redirect_uri=' +
-      encodeURIComponent(redirect_uri)
+router.get('/login', cors(), (req, res) => {
+  const scopes = 'playlist-modify-private  ';
+  res.header('Access-Control-Allow-Origin', '*');
+  res.set('content-type', 'text/plain');
+  res.send(
+    'https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: spotify.creds.clientId,
+        scopes: scopes,
+        // redirect_uri: encodeURIComponent(spotify.creds.redirectUri),
+        redirect_uri: spotify.creds.redirectUri,
+      })
   );
 });
 
