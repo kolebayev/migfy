097ea8efec59c3b975a517dbfde63e37e5d91a32
Ilya Kolebayev <kolebayev@gmail.com>
2020-06-11 17:20:20 +0300

    Update server

--- server/routes/callback.js
+++ server/routes/callback.js
@@ -4,14 +4,62 @@ const router = express.Router();
 const cors = require('cors');
 const spotify = require('../spotify/creds.js');
 
-// console.log('callback route works')
+console.log('callback route works');
 
 router.get('/callback', cors(), (req, res) => {
-    // этот консоль лог нихуя не работает
-    const code = req.query.code || null
-    console.log('code: ', code)
-    res.redirect('http://localhost:3000')
-    // console.log('callback route works')
+  const code = req.query.code || null;
+  console.log('code: ', code);
+  //   console.log(req.query.state);
+  res.redirect('http://localhost:3000');
+
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: spotify.creds.redirectUri,
+      grant_type: 'authorization_code',
+    },
+    headers: {
+      Authorization:
+        'Basic ' +
+        new Buffer(spotify.creds.clientId + ':' + spotify.creds.clientSecret).toString('base64'),
+    },
+    json: true,
+  };
+
+  req.post(authOptions, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token,
+        refresh_token = body.refresh_token;
+
+      var options = {
+        url: 'https://api.spotify.com/v1/me',
+        headers: { Authorization: 'Bearer ' + access_token },
+        json: true,
+      };
+
+      // use the access token to access the Spotify Web API
+      req.get(options, function (error, response, body) {
+        console.log(body);
+      });
+
+      // we can also pass the token to the browser to make requests from there
+      //   res.redirect(
+      //     '/#' +
+      //       querystring.stringify({
+      //         access_token: access_token,
+      //         refresh_token: refresh_token,
+      //       })
+      //   );
+    } else {
+      res.redirect(
+        '/#' +
+          querystring.stringify({
+            error: 'invalid_token',
+          })
+      );
+    }
+  });
 });
 
 module.exports = router;
--- server/routes/login.js
+++ server/routes/login.js
@@ -8,13 +8,13 @@ router.get('/login', cors(), (req, res) => {
   const scopes = 'playlist-modify-private playlist-modify-public';
   res.header('Access-Control-Allow-Origin', '*');
   res.send(
-    // res.redirect(
     'https://accounts.spotify.com/authorize?' +
       querystring.stringify({
         response_type: 'code',
         client_id: spotify.creds.clientId,
         scopes: scopes,
         redirect_uri: spotify.creds.redirectUri,
+        // state: '123',
       })
   );
 });
