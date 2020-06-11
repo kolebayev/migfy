164f287974bfd31e074b6614dd7d8294979174cd
Ilya Kolebayev <kolebayev@gmail.com>
2020-06-11 14:33:43 +0300

    Update callback.js

--- server/routes/callback.js
+++ server/routes/callback.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const cors = require('cors');
 const spotify = require('../spotify/creds.js');
 
-console.log('callback route works')
+// console.log('callback route works')
 
 router.get('/callback', cors(), (req, res) => {
     // этот консоль лог нихуя не работает
