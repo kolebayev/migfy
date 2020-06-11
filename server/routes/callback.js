0caede346547e53848ce756c74aa8b55fea1c3c2
Ilya Kolebayev <kolebayev@gmail.com>
2020-06-11 22:06:51 +0300

    Fix

--- server/routes/callback.js
+++ server/routes/callback.js
@@ -3,63 +3,80 @@ const querystring = require('querystring');
 const router = express.Router();
 const cors = require('cors');
 const spotify = require('../spotify/creds.js');
+const https = require('https');
+// const axios = require('axios');
 
 console.log('callback route works');
 
 router.get('/callback', cors(), (req, res) => {
   const code = req.query.code || null;
   console.log('code: ', code);
-  //   console.log(req.query.state);
-  res.redirect('http://localhost:3000');
+  res.redirect('http://localhost:3000/');
 
-  var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    form: {
-      code: code,
-      redirect_uri: spotify.creds.redirectUri,
-      grant_type: 'authorization_code',
-    },
+  var authHeader =
+    'Basic ' +
+    Buffer.from(spotify.creds.clientId + ':' + spotify.creds.clientSecret).toString('base64');
+
+  const authOptions = {
+    hostname: 'accounts.spotify.com',
+    path: '/api/token',
+    method: 'POST',
+    port: '443',
+    grant_type: 'authorization_code',
+    code: code,
+    redirect_uri: spotify.creds.redirectUri,
     headers: {
-      Authorization:
-        'Basic ' +
-        new Buffer(spotify.creds.clientId + ':' + spotify.creds.clientSecret).toString('base64'),
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: authHeader,
     },
-    json: true,
   };
 
-  request.post(authOptions, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      var access_token = body.access_token,
-        refresh_token = body.refresh_token;
+  const request = https
+    .request(authOptions, (res) => {
+      console.log('https request to get token ' + res.statusCode);
+      if (res.statusCode === 200) {
+        console.log('res200');
+      }
+    })
+    .on('error', (err) => {
+      console.log('Error: ', err.message);
+    });
 
-      var options = {
-        url: 'https://api.spotify.com/v1/me',
-        headers: { Authorization: 'Bearer ' + access_token },
-        json: true,
-      };
+  request.end();
 
-      // use the access token to access the Spotify Web API
-      request.get(options, function (error, response, body) {
-        console.log(body);
-      });
+  //   request.post(authOptions, (error, response, body) => {
+  //     if (!error && response.statusCode === 200) {
+  //       var access_token = body.access_token,
+  //         refresh_token = body.refresh_token;
 
-      // we can also pass the token to the browser to make requests from there
-      //   res.redirect(
-      //     '/#' +
-      //       querystring.stringify({
-      //         access_token: access_token,
-      //         refresh_token: refresh_token,
-      //       })
-      //   );
-    } else {
-      res.redirect(
-        '/#' +
-          querystring.stringify({
-            error: 'invalid_token',
-          })
-      );
-    }
-  });
+  //       var options = {
+  //         url: 'https://api.spotify.com/v1/me',
+  //         headers: { Authorization: 'Bearer ' + access_token },
+  //         json: true,
+  //       };
+
+  //       // use the access token to access the Spotify Web API
+  //       request.get(options, function (error, response, body) {
+  //         console.log(body);
+  //       });
+
+  //       // we can also pass the token to the browser to make requests from there
+  //       //   res.redirect(
+  //       //     '/#' +
+  //       //       querystring.stringify({
+  //       //         access_token: access_token,
+  //       //         refresh_token: refresh_token,
+  //       //       })
+  //       //   );
+  //     } else {
+  //       res.redirect(
+  //         '/#' +
+  //           querystring.stringify({
+  //             error: 'invalid_token',
+  //           })
+  //       );
+  //     }
+  //   });
 });
 
 module.exports = router;
